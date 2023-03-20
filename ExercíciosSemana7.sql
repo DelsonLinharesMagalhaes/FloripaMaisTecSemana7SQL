b81eaee2e4dc997264cf92635fe1b7acb78830ab
Delson Magalhaes <delson_magalhaes@estudante.sesisenai.org.br>
2023-03-20 15:42:31 -0300

    secondt commit

---
+++ ExercíciosSemana7.sql
@@ -0,0 +1,12 @@
+-- EXERCÍCIOS SEMANA 7 SQL
+
+CREATE DATABASE SEMANA7;
+
+USE SEMANA7;
+
+-- Ex 1 - Crie uma Tabela chamada clientes
+
+CREATE TABLE CLIENTES(Id INT,
+					Nome VARCHAR(50),
+					Telefone VARCHAR(11),
+					Endereco VARCHAR(100))
